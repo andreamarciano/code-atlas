fc3b8971595b2965cb106ea7b89cb24d93d3047e
andreamarciano <andrea.marciano.66@gmail.com>
2025-06-20 22:40:25 +0200

    frontend
    topbar refactor -> signedinmenu

---
+++ frontend/src/components/Topbar/SignedInMenu.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { CircleUser, ChevronDown, LogOut, Settings } from "lucide-react";
+
+import { useUser } from "../../utils/UserContext";
+
+export default function SignedInMenu() {
+  /* Auth User */
+  const { user, logout } = useUser();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={toggleMenu}
+        className="flex items-center gap-2 text-white hover:bg-neutral-800 rounded-2xl cursor-pointer"
+      >
+        <CircleUser />
+        <span className="text-sm text-yellow-400">{user?.username}</span>
+        <ChevronDown size={16} />
+      </button>
+
+      {menuOpen && (
+        <div className="absolute right-0 mt-2 w-44 bg-neutral-700 rounded-xl shadow-xl z-50 overflow-hidden border border-neutral-600">
+          <button className="flex items-center gap-2 w-full px-4 py-2 text-sm text-white hover:bg-neutral-600 transition-colors cursor-pointer">
+            <CircleUser size={18} />
+            Profile
+          </button>
+          <button className="flex items-center gap-2 w-full px-4 py-2 text-sm text-white hover:bg-neutral-600 transition-colors cursor-pointer">
+            <Settings size={18} />
+            Settings
+          </button>
+          <button
+            onClick={logout}
+            className="flex items-center gap-2 w-full px-4 py-2 text-sm text-red-400 hover:bg-neutral-800 transition-colors cursor-pointer"
+          >
+            <LogOut size={18} className="-rotate-180" />
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
